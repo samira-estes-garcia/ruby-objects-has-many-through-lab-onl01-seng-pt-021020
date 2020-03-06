595824d80a90a16a9649dda05bef74c0ffa85a8b
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-06 03:20:57 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -2,12 +2,20 @@ class Patient
   
   attr_accessor :name 
   
+  @@all = []
+  
   def initialize(name)
     @name = name
+    @@all << self
   end
   
   def new_appointment(doctor, date)
     Appointment.new(date, self, doctor)
+  end
+  
+  
+  def self.all 
+    @@all 
   end
   
 end
