ce5dd931d43847ed0759b4b87a2c941e37b6469e
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-06 03:14:01 +0000

    Automatically backed up by Learn

---
+++ lib/appointment.rb
@@ -0,0 +1,11 @@
+class Appointment
+  
+  attr_accessor :date, :patient, :doctor 
+  
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+  end
+  
+end
