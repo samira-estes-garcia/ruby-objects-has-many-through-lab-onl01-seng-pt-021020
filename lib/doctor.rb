e1817a984545da48d573acdc942f8225f7e91fc7
Samira Estes-Garcia <samira.estes.garcia@gmail.com>
2020-03-06 03:14:45 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -14,11 +14,11 @@ class Doctor
   end
   
   def new_appointment(date, patient)
-    Appointment.new()
-    
+    Appointment.new(date, patient, self)
   end  
   
   def patients 
+    
   end
 
 end
